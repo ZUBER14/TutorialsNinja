932ea1d0c6bb5e89d6cd7e285d2335e4c8709a44
zuber14 <justgamepubg@gmail.com>
2023-02-20 12:07:36 +0530

    updated

--- src/main/java/com/tutorialsninja/qa/utilities/BaseClass.java
+++ src/main/java/com/tutorialsninja/qa/utilities/BaseClass.java
@@ -76,7 +76,7 @@ public class BaseClass {
 	public static String generateEmailWithTimeStamp() {
 		Date date = new Date();
 		String timestamp = date.toString().replace(" ", "_").replace(":", "_");
-		return "amotoori" + timestamp + "@gmail.com";
+		return "zubertest0" + timestamp + "@gmail.com";
 	}
 
 	/**
